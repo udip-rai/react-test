88824003ac39462e0a10e81266b219e1eab7036b
Udip Rai <atomicdeveloper0329@gmail.com>
2024-01-08 07:01:40 +0545

    fixing netlify redirect issue

--- src/views/pages/Home.tsx
+++ src/views/pages/Home.tsx
@@ -1,6 +1,14 @@
 const Home = () => {
   return (
-    <div>Deployed in Netlify here: https://react-test-udip.netlify.app/</div>
+    <div>
+      <p className="font-bold text-blue-500">Deployed in Netlify here:</p>
+      <a
+        href="https://react-test-udip.netlify.app/"
+        className="hover:text-red-600"
+      >
+        https://react-test-udip.netlify.app/
+      </a>
+    </div>
   );
 };
 
